0c9af94d37f51e705be4fd9336bf9bd560d499ef
Hubert Tockner <hubert.tockner@tugraz.at>
2018-06-06 21:24:54 +0200

    Add Hello World

--- 1_Hello_World/1_Hello_World.cpp
+++ 1_Hello_World/1_Hello_World.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 
 int main(int argc,char** argv){
-<<<<<<< HEAD:1_Hello_World/1_Hello_World.gcc
-	std::cout("Hello World");
-=======
+
 	std::cout << "Hello World";
->>>>>>> master:1_Hello_World/1_Hello_World.cpp
+
 	return 0;
 }
